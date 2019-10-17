1ff97c4286a7de897b3eb418ea4ba0103bf1126e
fmastjik <56566344+fmastjik@users.noreply.github.com>
2019-10-17 12:41:43 -0500

    Update Plivo_SMS.py

--- Plivo_SMS.py
+++ Plivo_SMS.py
@@ -2,7 +2,11 @@ import plivo
 
 client = plivo.RestClient()
 message = client.messages.create(
-    src='+12192173175',
-    dst='+18328535174',
-    text='This is a test'
+   src='+12192173175',
+   dst='+18328535174',
+   text='This is a test'
 )
+uuid= message.message_uuid
+uuid= str(uuid[0])
+response = client.messages.get(message_uuid=uuid,)
+print (response)
